e1e5528dc9b3453f0a4883b1ffbb68e94c53c8c3
Chirag Mehta <chiragmehta.uk@gmail.com>
2021-12-06 16:23:13 +0000

    imported and exported node files

---
+++ collection.js
@@ -0,0 +1,23 @@
+let myCollection = [
+    {
+      name: "School of Code mug",
+      count: 1,
+      whatILike: "It has my cute pixel character on it!"
+    },
+    {
+      name: "School of Code hat",
+      count: 2,
+      whatILike: "An often overlooked fashion accessory"
+    },
+    {
+      name: "School of Code pillow",
+      count: 1,
+      whatILike: "Eat. Sleep. Code. Repeat :)"
+    }
+  ];
+
+  export default myCollection;
+
+//   👉 3b. Use Node's default exporting syntax in collection.js to export the myCollection variable so that it's ready to import elsewhere in your folder. To use this new import/export syntax, we need to change our package.json so that it has the property "type": "module".
+
+// Remember that you can check Google if you can't remember the syntax, but we are using ES6 import/export syntax!
--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-let myCollection = [
-    {
-      name: "School of Code mug",
-      count: 1,
-      whatILike: "It has my cute pixel character on it!"
-    },
-    {
-      name: "School of Code hat",
-      count: 2,
-      whatILike: "An often overlooked fashion accessory"
-    },
-    {
-      name: "School of Code pillow",
-      count: 1,
-      whatILike: "Eat. Sleep. Code. Repeat :)"
-    }
-  ];
-  console.log(myCollection);
-
+import myCollection from "./collection.js";
 
   function describeItem(item) {
     if (item.count === 1) {
