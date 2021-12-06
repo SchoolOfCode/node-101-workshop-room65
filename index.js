03f87a32cc0fb8db361c80a2b131be1efff7a8e3
Chirag Mehta <chiragmehta.uk@gmail.com>
2021-12-06 16:47:32 +0000

    installed crappy npm toSnakeCase package

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import chalk from 'chalk';
 
 import myCollection from './collection.js';
 
-
+import toSnakeCase from 'to-snake-case';
 
   function describeItem(item) {
     if (item.count === 1) {
-        console.log(`I have a ${chalk.cyan(item.name)}. Here's what I like about it: ${chalk.green(item.whatILike)}.`);
+        console.log(toSnakeCase(`I have a ${chalk.cyan(item.name)}. Here's what I like about it: ${chalk.green(item.whatILike)}.`));
     } else {
-        console.log(`I have ${chalk.yellow(item.count)} ${chalk.cyan(item.name)}s. Here's what I like about them: ${chalk.green(item.whatILike)}`);
+        console.log(`I have ${chalk.yellow.bold(item.count)} ${chalk.cyan.underline(item.name)}s. Here's what I like about them: ${chalk.green(item.whatILike)}`);
     }
   }
 
